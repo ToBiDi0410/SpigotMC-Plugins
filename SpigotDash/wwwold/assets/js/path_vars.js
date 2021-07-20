3429736ec0dd43ea5a6cca47af249162a1588d03
BuildTools <unconfigured@null.spigotmc.org>
2021-07-20 14:07:47 +0200

    Cleaned up Repository

--- SpigotDash/wwwold/assets/js/path_vars.js
+++
@@ -1,5 +0,0 @@
-var url = window.location.href;
-var RELATIVE_PATH = url.split("/")[0] + "//" + url.split("/")[2] + "/";
-//RELATIVE_PATH = "http://localhost:9677/";
-var API_URL = RELATIVE_PATH + "api";
-var AUTH_URL = RELATIVE_PATH + "auth"
